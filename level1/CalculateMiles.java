553bf44b2122d711e07ad800076b67ac89395c49
Harsh2210991612 <harsh1612.be22@chitkara.edu.in>
2025-08-21 11:23:39 +0530

    Added: Calculating miles

---
+++ level1/CalculateMiles.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class calculateMiles{
+    public static void main(String [] args){
+	    Scanner sc = new Scanner(System.in);
+		double kilometer = sc.nextDouble();
+		double mile=1.6;
+		double distIntoKilometer= kilometer*mile;
+		System.out.print("The total miles is" +distIntoKilometer+"mile for the given "+kilometer+"km");	
+    }
+}
